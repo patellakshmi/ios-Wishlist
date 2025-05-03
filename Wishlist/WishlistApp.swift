4a0ac779aa3b84b597467a67494da5d3f3036b0b
patellakshmi <er.lspatel@gmail.com>
2025-05-03 23:35:57 +0530

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by RE on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
